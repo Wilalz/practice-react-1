d1a48864b4a4100e32ec9d070ce4805afa819911
WilAlz <wilalzatep@gmail.com>
2023-04-30 03:50:51 -0500

    [FEAT] Add Card 12 Formulario del examen parcial de DH

--- src/components/ejercicio12/Examen-parcial-DH/FormParcial.jsx
+++ src/components/ejercicio12/Examen-parcial-DH/FormParcial.jsx
@@ -5,7 +5,7 @@ export const FormParcial = () => {
     return (
         <div className={styles.container}>
             <h2>12. Form Examen Parcial DH</h2>
-            <p className={styles.error_msg}> *** Agregar el form realizado para el examen parcial de DH ***</p>
+            <p className={styles.error_msg}> *** Agregar el Form realizado para el examen parcial de DH ***</p>
         </div>
     )
 }
